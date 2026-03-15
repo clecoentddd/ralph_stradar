1887a5096f39ee0f5ee248e8013a411e4906a4cb
clecoentddd <christophelecoent@gmail.com>
2026-03-15 19:13:59 +0100

    Fixed security for user account projection

--- stradar/src/main/kotlin/stradar/security/SecurityConfig.kt
+++ stradar/src/main/kotlin/stradar/security/SecurityConfig.kt
@@ -4,21 +4,41 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig {
 
   @Bean
   fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-
     http
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .csrf { it.disable() }
             .authorizeHttpRequests {
-              it.requestMatchers("/api/**", "/accountlist", "/account/**").authenticated()
-              it.anyRequest().permitAll()
+              it.requestMatchers(
+                              "/signin",
+                              "/signintoorganizationpersonaccount",
+                              "/signinsuperadmin"
+                      )
+                      .permitAll()
+              it.anyRequest().authenticated()
             }
             .oauth2ResourceServer { it.jwt {} }
 
     return http.build()
+  }
+
+  @Bean
+  fun corsConfigurationSource(): CorsConfigurationSource {
+    val config = CorsConfiguration()
+    config.allowedOriginPatterns = listOf("*")
+    config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    config.allowedHeaders = listOf("*")
+    config.allowCredentials = true
+    val source = UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration("/**", config)
+    return source
   }
 }
